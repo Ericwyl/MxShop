41c5ad170978f2c254190cc21694f711b3e7fd5d
wyl <weiyinlong725@163.com>
2022-10-24 21:56:22 +0800

    完善goods模块views代码，设置url

--- MxShop/urls.py
+++ MxShop/urls.py
@@ -9,6 +9,23 @@ from django.views.static import serve
 from MxShop.settings import MEDIA_ROOT
 from goods.view_base import GoodsListView
 from rest_framework.documentation import include_docs_urls
+from rest_framework.routers import DefaultRouter
+from goods.views import GoodsListViewSet, CategoryViewSet, BannerViewset, IndexCategoryViewset, HotSearchsViewset
+from django.views.generic import TemplateView
+
+
+router = DefaultRouter()
+
+#配置goods的url
+router.register(r'goods', GoodsListViewSet, base_name="goods")
+#配置Category的url
+router.register(r'categorys', CategoryViewSet, base_name="categorys")
+#配置首页轮播图的url
+router.register(r'banners', BannerViewset, base_name="banners")
+#配置热搜的url
+router.register(r'hotsearchs', HotSearchsViewset, base_name="hotsearchs")
+#首页系列商品展示url
+router.register(r'indexgoods', IndexCategoryViewset, base_name="indexgoods")
 
 
 urlpatterns = [
@@ -19,4 +36,10 @@ urlpatterns = [
     path('goods/', GoodsListView.as_view(), name='goods-list'),
     path('docs', include_docs_urls(title='天空八部之回首掏')),
     path('api-auth/', include('rest_framework.urls')),
+    #商品列表页
+    path('^', include(router.urls)),
+    #首页
+    path('index/', TemplateView.as_view(template_name='index.html'), name='index'),
+
+
 ]
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -9,7 +9,6 @@ from goods.serializers import GoodsSerializer, CategorySerializer, BannerSeriali
 from .models import Goods
 from rest_framework.response import Response
 
-
 # class GoodsListView(View):
 #     '''
 #     商品列表
@@ -32,16 +31,23 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from .filters import GoodsFilter
 
+'''
+drf中的APIView,GenericView,viewsets和router原理：
+GenericViewSet
+mixins源码解析：
+
+'''
+
 
 class GoodsPagination(PageNumberPagination):
     '''商品列表自定义分页'''
-    #默认每页显示个数
+    # 默认每页显示个数
     page_size = 12
-    #可以动态改变每页显示的个数
+    # 可以动态改变每页显示的个数
     page_size_query_param = 'page_size'
-    #页码参数
+    # 页码参数
     page_query_param = 'page'
-    #最多能显示多少页
+    # 最多能显示多少页
     max_page_size = 100
 
 
@@ -51,21 +57,25 @@ class GoodsListViewSet(CacheResponseMixin, mixins.ListModelMixin, mixins.Retriev
     retrieve:获取商品详情
     '''
     throttle_classes = (UserRateThrottle, AnonRateThrottle)
-    #这里必须定义一个默认的排序，否则 会报错
+    # 这里必须定义一个默认的排序，否则 会报错
     queryset = Goods.objects.all().order_by('id')
-    #分页
+    # 分页
     pagination_class = GoodsPagination
-    #序列化
+    # 序列化
     serializer_class = GoodsSerializer
     filter_backends = (DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter)
 
-    #设置filter的类为我们自定义的类
-    #过滤
+    # 设置filter的类为我们自定义的类
+    # 搜索
+    search_fields = ('name', 'goods_brief', 'goods_desc')
+
+    # 过滤
     filter_class = GoodsFilter
-    #搜索
+
+    # 排序
     ordering_fields = ('sold_num', 'shop_price')
 
-    #商品点击+1
+    # 商品点击+1
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.click_num += 1
@@ -93,16 +103,33 @@ class BannerViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = BannerSerializer
 
 
-class GoodsListView(mixins.ListModelMixin, generics.GenericAPIView):
-    '商品列表页'
-    queryset = Goods.objects.all()
-    serializer_class = GoodsSerializer
+# class GoodsListView(mixins.ListModelMixin, generics.GenericAPIView):
+#     '商品列表页'
+#     queryset = Goods.objects.all()
+#     serializer_class = GoodsSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+from .serializers import IndexCategorySerializer
 
 
+class IndexCategoryViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
+    '''
+    首页商品分类数据
+    '''
+    # 获取is_True(导航栏)里面的分类下的商品数据
+    queryset = GoodsCategory.objects.filter(is_tab=True, name__in=["生鲜食品", "酒水饮料"])
+    serializer_class = IndexCategorySerializer
 
 
+from .models import HotSearchWords
+from .serializers import HotWordsSerializer
+
+
+class HotSearchsViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
+    '''热搜'''
+    queryset = HotSearchWords.objects.all().order_by("-index")
+    serializer_class = HotWordsSerializer
 
 
